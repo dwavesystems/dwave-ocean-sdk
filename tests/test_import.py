43f5617c707573113f36eaea2e6d6716662fbcc7
Alexander Condello <acondello@dwavesys.com>
2018-05-10 16:33:52 -0700

    Rename to dwave-ocean-sdk

---
+++ tests/test_import.py
@@ -0,0 +1,9 @@
+# import each package and test that they are all there
+import dimod
+import dwavebinarycsp
+import dwave.cloud.client
+import dwave_networkx
+import dwave.system
+import penaltymodel
+import minorminer
+import dwave_qbsolv
