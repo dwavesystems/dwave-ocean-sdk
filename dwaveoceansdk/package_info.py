102d4b27bd1f12b170476f3e5ddd348219c3c353
Alexander Condello <acondello@dwavesys.com>
2018-05-31 14:24:45 -0700

    Update version 0.3.1 -> 1.0.0

--- dwaveoceansdk/package_info.py
+++ dwaveoceansdk/package_info.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.1'
+__version__ = '1.0.0'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'tools@dwavesys.com'
 __description__ = 'Software development kit for open source D-Wave tools'
