2205d2d93990617970a3e45ce1f0bbab1292ce97
Radomir Stevanovic <radomir@dwavesys.com>
2019-03-25 13:22:33 -0700

    Update version to 1.3.0

--- dwaveoceansdk/package_info.py
+++ dwaveoceansdk/package_info.py
@@ -13,7 +13,7 @@
 #    limitations under the License.
 #
 # ================================================================================================
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'tools@dwavesys.com'
 __description__ = 'Software development kit for open source D-Wave tools'
