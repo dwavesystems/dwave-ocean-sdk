94d7a4ba48f37cf840c192af6fc9cdaee8a585be
Radomir Stevanovic <radomir@dwavesys.com>
2019-09-26 18:05:00 -0700

    Update to use the latest stable Ocean
    dwave-system -> 0.8.x
    dwave-hybrid -> 0.4.x

--- setup.py
+++ setup.py
@@ -30,9 +30,9 @@ else:
 
 install_requires = [
     'dwave-networkx>=0.8.0,<0.9.0',
-    'dwave-system>=0.7.0,<0.8.0',
+    'dwave-system>=0.8.0,<0.9.0',
     'dwave-qbsolv>=0.2.7,<0.3.0',
-    'dwave-hybrid>=0.3.0,<0.4.0',
+    'dwave-hybrid>=0.4.0,<0.5.0',
     'dwave-neal>=0.5.0,<0.6.0',
     'dwave-tabu>=0.2.0,<0.3.0',
     'dimod>=0.8.0,<0.9.0',
