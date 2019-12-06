82637e761f1f7954fc9c3b33b16b01cc321f35dd
Alexander Condello <arcondello@gmail.com>
2019-12-06 09:51:07 -0800

    Add dwave-neal 0.5.1 as a submodule

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ install_requires = [
     'dwave-system==0.8.0',
     'dwave-qbsolv>=0.2.7,<0.3.0',
     'dwave-hybrid==0.4.1',
-    'dwave-neal>=0.5.0,<0.6.0',
+    'dwave-neal==0.5.1',
     'dwave-tabu>=0.2.0,<0.3.0',
     'dimod>=0.8.0,<0.9.0',
     'numpy<1.16.0',     # only while we support py34
