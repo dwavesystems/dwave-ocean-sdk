eeea7334d3975eee7adb58ecafb47a5834901ccc
Alexander Condello <arcondello@gmail.com>
2019-12-06 09:56:15 -0800

    Add dwave-tabu 0.2.1 as a submodule

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ install_requires = [
     'dwave-qbsolv>=0.2.7,<0.3.0',
     'dwave-hybrid==0.4.1',
     'dwave-neal==0.5.1',
-    'dwave-tabu>=0.2.0,<0.3.0',
+    'dwave-tabu==0.2.1',
     'dimod>=0.8.0,<0.9.0',
     'numpy<1.16.0',     # only while we support py34
     'pyqubo>=0.3.0',
