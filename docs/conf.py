fd202ddb8024b316943af025903e9b066707404d
jpasvolsky <jpasvolsky@dwavesys.com>
2020-01-17 10:54:48 -0800

    Add dummy files for build warnings

--- docs/conf.py
+++ docs/conf.py
@@ -81,7 +81,7 @@ add_module_names = False
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
