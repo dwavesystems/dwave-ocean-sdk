2fe257a5d3a92a4e78760d83f3cc163bc885597f
arcondello <arcondello@gmail.com>
2020-02-07 14:24:51 -0800

    Support python 3.5+

--- setup.py
+++ setup.py
@@ -15,17 +15,11 @@
 # ================================================================================================
 from __future__ import absolute_import
 
-import sys
 from setuptools import setup
-
-_PY2 = sys.version_info.major == 2
 
 # add __version__, __author__, __authoremail__, __description__ to this namespace
 # equivalent to:
-if _PY2:
-    execfile("./dwaveoceansdk/package_info.py")
-else:
-    exec(open("./dwaveoceansdk/package_info.py").read())
+exec(open("./dwaveoceansdk/package_info.py").read())
 
 
 install_requires = [
@@ -46,10 +40,10 @@ install_requires = [
 ]
 
 extras_require = {
-    ':(platform_machine == "x86_64" or platform_machine == "amd64" or platform_machine == "AMD64") and python_version != "3.4"': [
+    ':(platform_machine == "x86_64" or platform_machine == "amd64" or platform_machine == "AMD64")': [
         'penaltymodel-mip==0.2.1'
     ],
-    ':platform_machine != "x86_64" and platform_machine != "amd64" and platform_machine != "AMD64" or python_version == "3.4"': [
+    ':platform_machine != "x86_64" and platform_machine != "amd64" and platform_machine != "AMD64"': [
         'penaltymodel-maxgap==0.5.1'
     ]
 }
@@ -60,16 +54,13 @@ packages = ['dwaveoceansdk']
 classifiers = [
     'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7'
 ]
 
-python_requires = '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*'
+python_requires = '>=3.5'
 
 setup(
     name='dwave-ocean-sdk',
