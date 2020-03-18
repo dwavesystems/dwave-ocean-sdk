af817cd99de528d8cf995089ed43ee5d9a2f7b11
Alexander Condello <arcondello@gmail.com>
2020-03-18 12:44:42 -0700

    Update dwave-networkx to 0.8.5

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ exec(open("./dwaveoceansdk/package_info.py").read())
 
 install_requires = [
     'dwave-cloud-client==0.7.0',
-    'dwave-networkx==0.8.4',
+    'dwave-networkx==0.8.5',
     'dwave-system==0.9.1',
     'dwave-qbsolv==0.3.0',
     'dwave-hybrid==0.4.2',
