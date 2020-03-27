90c669570cabc695bf8825ae0cea8a4e6bf5297b
Alexander Condello <arcondello@gmail.com>
2020-03-27 14:19:22 -0700

    Update dwave-system to 0.9.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ exec(open("./dwaveoceansdk/package_info.py").read())
 install_requires = [
     'dwave-cloud-client==0.7.1',
     'dwave-networkx==0.8.6',
-    'dwave-system==0.9.1',
+    'dwave-system==0.9.2',
     'dwave-qbsolv==0.3.0',
     'dwave-hybrid==0.4.3',
     'dwave-neal==0.5.4',
