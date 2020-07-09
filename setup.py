99929244f471b6f7326d7a6be60b334bd0816532
Radomir Stevanovic <radomir@dwavesys.com>
2020-07-09 06:41:57 -0700

    Order install requirements alphabetically
    Use the same order in changelog.

--- setup.py
+++ setup.py
@@ -23,21 +23,21 @@ exec(open("./dwaveoceansdk/package_info.py").read())
 
 
 install_requires = [
-    'dwave-cloud-client==0.7.3',
-    'dwave-networkx==0.8.6',
-    'dwave-system==0.9.6',
-    'dwave-qbsolv==0.3.0',
-    'dwave-hybrid==0.5.0',
-    'dwave-neal==0.5.4',
-    'dwave-tabu==0.3.0',
     'dimod==0.9.3',
+    'dwave-cloud-client==0.7.3',
+    'dwave-hybrid==0.5.0',
+    'dwave-inspector==0.2.2',
+    'dwave-neal==0.5.4',
+    'dwave-networkx==0.8.6',
+    'dwave-qbsolv==0.3.0',
+    'dwave-system==0.9.6',
+    'dwave-tabu==0.3.0',
     'dwavebinarycsp==0.1.2',
     'minorminer==0.1.9',
-    'penaltymodel==0.16.3',
     'penaltymodel-cache==0.4.1',
     'penaltymodel-lp==0.1.3',
+    'penaltymodel==0.16.3',
     'pyqubo>=0.4.0',
-    'dwave-inspector==0.2.2',
 ]
 
 # note: when updating the version of maxgap, it also must be updated in
