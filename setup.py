4a16696bff301e263cf12086457e85b5ffd7b04d
Radomir Stevanovic <radomir@dwavesys.com>
2020-07-24 11:09:50 -0700

    Update dimod from 0.9.4 to 0.9.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ exec(open("./dwaveoceansdk/package_info.py").read())
 
 
 install_requires = [
-    'dimod==0.9.4',
+    'dimod==0.9.5',
     'dwave-cloud-client==0.7.5',
     'dwave-hybrid==0.5.1',
     'dwave-inspector==0.2.2',
