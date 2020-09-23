a6c973f3d4f80268788e5dcb9592a678dc4f61d6
jpasvolsky <jpasvolsky@dwavesys.com>
2020-09-22 17:16:41 -0700

    Update dwave-system version to 1.0.0 fix next doctest failures

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ install_requires = [
     'dwave-neal==0.5.4',
     'dwave-networkx==0.8.7',
     'dwave-qbsolv==0.3.0',
-    'dwave-system==0.9.13',
+    'dwave-system==1.0.0',
     'dwave-tabu==0.3.0',
     'dwavebinarycsp==0.1.2',
     'minorminer==0.2.0',
