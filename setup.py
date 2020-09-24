42958e0bd472623711d2148902559263c9aaf49a
Radomir Stevanovic <radomir@dwavesys.com>
2020-09-23 18:23:44 -0700

    Update dwave-greedy from 0.1.0 to 0.1.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ exec(open("./dwaveoceansdk/package_info.py").read())
 install_requires = [
     'dimod==0.9.6',
     'dwave-cloud-client==0.8.1',
-    'dwave-greedy==0.1.0',
+    'dwave-greedy==0.1.1',
     'dwave-hybrid==0.6.0',
     'dwave-inspector==0.2.4',
     'dwave-neal==0.5.6',
