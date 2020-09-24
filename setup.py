71839612b0e5eb60870b0ba2ba46bccbdf751088
Alexander Condello <arcondello@gmail.com>
2020-09-23 20:22:22 -0700

    Update dimod from 0.9.6 to 0.9.7

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ exec(open("./dwaveoceansdk/package_info.py").read())
 
 
 install_requires = [
-    'dimod==0.9.6',
+    'dimod==0.9.7',
     'dwave-cloud-client==0.8.1',
     'dwave-greedy==0.1.1',
     'dwave-hybrid==0.6.0',
