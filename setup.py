e5a3fd1d588a62412fd8b1564e4cfda91ef20a94
Radomir Stevanovic <radomir@dwavesys.com>
2020-09-25 06:01:17 -0700

    Update dwave-system from 1.1.0 to 1.1.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ install_requires = [
     'dwave-neal==0.5.6',
     'dwave-networkx==0.8.8',
     'dwave-qbsolv==0.3.1',
-    'dwave-system==1.1.0',
+    'dwave-system==1.1.1',
     'dwave-tabu==0.3.0',
     'dwavebinarycsp==0.1.2',
     'minorminer==0.2.3',
