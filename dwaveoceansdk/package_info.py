c6bec85b3e31eb3ad8d4cd35f6958b118c355ef9
Radomir Stevanovic <radomir@dwavesys.com>
2020-09-28 05:49:38 -0700

    Release 3.0.1
    Fixes
    ---
    
    - pyqubo pinned to 0.4.0

--- dwaveoceansdk/package_info.py
+++ dwaveoceansdk/package_info.py
@@ -12,7 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-__version__ = '3.0.0'
+__version__ = '3.0.1'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'tools@dwavesys.com'
 __description__ = 'Software development kit for open source D-Wave tools'
