fb1de0794be5784026f7f4c7a670486b302e9a9e
Radomir Stevanovic <radomir@dwavesys.com>
2020-12-09 09:38:00 -0800

    Update dwave-cloud-client from 0.8.1 to 0.8.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ exec(open("./dwaveoceansdk/package_info.py").read())
 
 install_requires = [
     'dimod==0.9.12',
-    'dwave-cloud-client==0.8.1',
+    'dwave-cloud-client==0.8.2',
     'dwave-greedy==0.1.2',
     'dwave-hybrid==0.6.0',
     'dwave-inspector==0.2.4',
