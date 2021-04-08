0e28aed920a50944f1003f2964f9eefe500bf283
jpasvolsky <jpasvolsky@dwavesys.com>
2021-04-08 08:08:43 -0700

    Update to Python 3 URL

--- docs/conf.py
+++ docs/conf.py
@@ -283,7 +283,7 @@ epub_exclude_files = ['search.html']
 # Example configuration for intersphinx: refer to the Python standard library.
 #intersphinx_mapping = {'https://docs.python.org/': None,
 #                       'http://networkx.readthedocs.io/en/latest/': None}
-intersphinx_mapping = {'python': ('https://docs.python.org/', None),
+intersphinx_mapping = {'python': ('https://docs.python.org/3/', None),
     'numpy': ('http://docs.scipy.org/doc/numpy/', None),
     'bson': ('https://api.mongodb.com/python/current/', None),
     'networkx': ('https://networkx.github.io/documentation/stable/', None),
