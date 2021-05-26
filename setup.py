37eab9bba3ca5f349d4f247d06576aa8fd9c0b70
Heidi Tong <htong@dwavesys.com>
2021-05-26 16:56:32 -0700

    Update dimod from 0.9.13 to 0.9.15

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ exec(open("./dwaveoceansdk/package_info.py").read())
 
 
 install_requires = [
-    'dimod==0.9.13',
+    'dimod==0.9.15',
     'dwave-cloud-client==0.8.4',
     'dwave-greedy==0.1.2',
     'dwave-hybrid==0.6.1',
