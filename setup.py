5456442778b80c23624a88300c617c756a52df98
Radomir Stevanovic <radomir@dwavesys.com>
2021-10-04 10:39:06 -0700

    Update dimod from 0.10.6 to 0.10.7

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ exec(open("./dwaveoceansdk/package_info.py").read())
 
 
 install_requires = [
-    'dimod==0.10.6',
+    'dimod==0.10.7',
     'dwave-cloud-client==0.9.0',
     'dwave-greedy==0.2.1',
     'dwave-hybrid==0.6.3',
