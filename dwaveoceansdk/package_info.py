249a1f5227da91170ea11b54e60500f64fffe589
Radomir Stevanovic <radomir@dwavesys.com>
2021-10-04 12:30:23 -0700

    Release version 4.1.0

--- dwaveoceansdk/package_info.py
+++ dwaveoceansdk/package_info.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '4.0.0'
+__version__ = '4.1.0'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'tools@dwavesys.com'
 __description__ = 'Software development kit for open source D-Wave tools'
