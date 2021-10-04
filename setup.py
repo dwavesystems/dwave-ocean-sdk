a0f0e48b4344cbb1462cd602367e1e11eace2d24
Radomir Stevanovic <radomir@dwavesys.com>
2021-10-04 10:49:58 -0700

    Update dwave-networkx from 0.8.9 to 0.8.10

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ install_requires = [
     'dwave-hybrid==0.6.4',
     'dwave-inspector==0.2.7',
     'dwave-neal==0.5.8',
-    'dwave-networkx==0.8.9',
+    'dwave-networkx==0.8.10',
     'dwave-preprocessing==0.3.0',
     'dwave-qbsolv==0.3.3',
     'dwave-system==1.9.0',
