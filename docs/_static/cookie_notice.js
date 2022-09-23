d6717a4dd925bd66079ed01205ba862d90804725
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2022-09-23 09:59:34 -0700

    Add GTM

--- docs/_static/cookie_notice.js
+++ docs/_static/cookie_notice.js
@@ -1,4 +1,5 @@
 const send_ga = () => {
+//-- Universal analytics UA
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -6,6 +7,13 @@ const send_ga = () => {
 
   ga('create', 'UA-124167090-6', 'auto');
   ga('send', 'pageview');
+
+//-- GA4 with GTM
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayerGTM','GTM-TG454ZB');
 }
 
 let cookieAccepted = localStorage.cookieAccepted;
